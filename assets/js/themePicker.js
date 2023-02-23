0314493a982ed7b7751cc8f40a3ae422990d25c5
BlairCurrey <curreyb88@gmail.com>
2023-02-23 04:23:58 -0500

    feat: detect active theme

--- assets/js/themePicker.js
+++ assets/js/themePicker.js
@@ -1,84 +1,101 @@
 const ROOT = document.querySelector(':root');
-
-PALETTES = {
-  blue: {
-    '--blue-900': '#001A33',
-    '--blue-800': '#002E5E',
-    '--blue-700': '#00438C',
-    '--blue-600': '#0057B8',
-    '--cyan-800': '#007BC7',
-    '--indigo-900': '#1F1A36',
-    '--indigo-800': '#231E39',
-    '--indigo-700': '#1e2039',
-    '--indigo-600': '#2D2747',
-    '--bluegrey-100': '#B3B8CD',
-    '--green-300': '#a2df1a',
-    '--white-100': '#ffffff'
-  },
-  purple: {
-  '--purple-900': '#2d0036',
-  '--purple-800': '#4f015b',
-  '--purple-700': '#730283',
-  '--purple-600': '#9a02ac',
-  '--pink-800': '#C400D7',
-  '--indigo-900': '#1F1A36',
-  '--indigo-800': '#231E39',
-  '--indigo-700': '#1e2039',
-  '--indigo-600': '#2D2747',
-  '--bluegrey-100': '#B3B8CD',
-  '--amber-500': '#FEBB0B',
-  '--white-100': '#ffffff'
-  }
-};
+const STYLE = getComputedStyle(ROOT);
+const DEFAULT_THEME = 'blue'
 
 THEMES = {
   blue: {
-    '--bg-1': PALETTES.blue['--blue-900'],
-    '--bg-2': PALETTES.blue['--blue-800'],
-    '--bg-3': PALETTES.blue['--blue-700'],
-    '--bg-4': PALETTES.blue['--blue-600'],
-    '--bg-5': PALETTES.blue['--cyan-800'],
-    '--bg-card-1': PALETTES.blue['--indigo-900'],
-    '--bg-card-2': PALETTES.blue['--indigo-800'],
-    '--text-1': PALETTES.blue['--bluegrey-100'],
-    '--text-2': PALETTES.blue['--white-100'],
-    '--highlight': PALETTES.blue['--green-300'],
-    '--img-border': PALETTES.blue['--cyan-800'],
-    '--btn': PALETTES.blue['--cyan-800'],
-    '--border-skill': PALETTES.blue['--indigo-600']
+    '--bg-1': STYLE.getPropertyValue('--blue-900'),
+    '--bg-2': STYLE.getPropertyValue('--blue-800'),
+    '--bg-3': STYLE.getPropertyValue('--blue-700'),
+    '--bg-4': STYLE.getPropertyValue('--blue-600'),
+    '--bg-5': STYLE.getPropertyValue('--cyan-800'),
+    '--bg-card-1': STYLE.getPropertyValue('--indigo-900'),
+    '--bg-card-2': STYLE.getPropertyValue('--indigo-800'),
+    '--text-1': STYLE.getPropertyValue('--bluegrey-100'),
+    '--text-2': STYLE.getPropertyValue('--white-100'),
+    '--highlight': STYLE.getPropertyValue('--green-300'),
+    '--img-border': STYLE.getPropertyValue('--cyan-800'),
+    '--btn': STYLE.getPropertyValue('--cyan-800'),
+    '--border-skill': STYLE.getPropertyValue('--indigo-600')
   },
   purple: {
-    '--bg-1': PALETTES.purple['--purple-900'],
-    '--bg-2': PALETTES.purple['--purple-800'],
-    '--bg-3': PALETTES.purple['--purple-700'],
-    '--bg-4': PALETTES.purple['--purple-600'],
-    '--bg-5': PALETTES.purple['--pink-800'],
-    '--bg-card-1': PALETTES.purple['--indigo-900'],
-    '--bg-card-2': PALETTES.purple['--indigo-800'],
-    '--text-1': PALETTES.purple['--bluegrey-100'],
-    '--text-2': PALETTES.purple['--white-100'],
-    '--highlight': PALETTES.purple['--amber-500'],
-    '--img-border': PALETTES.purple['--pink-800'],
-    '--btn': PALETTES.purple['--pink-800'],
-    '--border-skill': PALETTES.purple['--indigo-600']
+    '--bg-1': STYLE.getPropertyValue('--purple-900'),
+    '--bg-2': STYLE.getPropertyValue('--purple-800'),
+    '--bg-3': STYLE.getPropertyValue('--purple-700'),
+    '--bg-4': STYLE.getPropertyValue('--purple-600'),
+    '--bg-5': STYLE.getPropertyValue('--pink-800'),
+    '--bg-card-1': STYLE.getPropertyValue('--indigo-900'),
+    '--bg-card-2': STYLE.getPropertyValue('--indigo-800'),
+    '--text-1': STYLE.getPropertyValue('--bluegrey-100'),
+    '--text-2': STYLE.getPropertyValue('--white-100'),
+    '--highlight': STYLE.getPropertyValue('--amber-500'),
+    '--img-border': STYLE.getPropertyValue('--pink-800'),
+    '--btn': STYLE.getPropertyValue('--pink-800'),
+    '--border-skill': STYLE.getPropertyValue('--indigo-600')
   }
 };
 
 // theme should be string representing key in THEMES
 function setTheme(themeKey){
   const { style } = ROOT;
-  // const newBG = getComputedStyle(ROOT).getPropertyValue('--bg-1')
   
   Object.keys(THEMES[themeKey]).forEach(cssVar => {
     style.setProperty(cssVar, THEMES[themeKey][cssVar]);
   })
 
   localStorage.setItem('themeKey', themeKey);
+
+  // Disable the button for the currently active theme
+  const buttons = document.querySelectorAll('.theme');
+  buttons.forEach(button => {
+    if (button.id === `${themeKey}-theme`) {
+      button.disabled = true;
+    } else {
+      button.disabled = false;
+    }
+  });
 };
 
+function loadThemeFromStorage(){
+  try {
+    const themeKey = localStorage.getItem('themeKey');
+    if(themeKey && themeKey in THEMES){
+      setTheme(themeKey);
+      return true;
+    };
+  } catch (error) {
+    console.error(error);
+  }
+  return false;
+}
+
+function renderThemePicker(){
+  const themePicker = document.getElementById('theme-picker');
+  const themeKeys = Object.keys(THEMES);
+
+  if (themeKeys.length === 0) return themePicker.style.display = 'none';
+
+  const i = document.createElement('i');
+  i.classList.add('fa-solid', 'fa-palette')
+  themePicker.append(i)
+
+  const activeTheme = localStorage.getItem('themeKey') || DEFAULT_THEME;
+
+  themeKeys.forEach((themeKey) => {
+    const button = document.createElement('button');
+    button.classList.add('theme');
+    button.id = `${themeKey}-theme`;
+    button.onclick = () => setTheme(themeKey);
+    themePicker.appendChild(button);
+
+    if(themeKey === activeTheme){
+      button.disabled = true
+    }
+  });
+}
+
 window.onload = () => {
-  const themeKey = localStorage.getItem('themeKey');
-  if(themeKey){
-    setTheme(themeKey);
-  };
+  const loaded = loadThemeFromStorage();
+  if(!loaded) setTheme(DEFAULT_THEME);
+  renderThemePicker();
 };
