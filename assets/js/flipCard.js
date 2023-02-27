e9b45844546740a4d146875ae3b7a63c37527db5
BlairCurrey <curreyb88@gmail.com>
2023-02-27 14:46:16 -0500

    feat: start snake game

--- assets/js/flipCard.js
+++ assets/js/flipCard.js
@@ -1,8 +1,12 @@
-const flipBtn = document.getElementById("flip-btn")
-const card = document.getElementById("card")
+const CARD = document.getElementById("card")
+const FLIP_BUTTON = renderFlipBtn();
 
-flipBtn.addEventListener("click", flipCard);
+FLIP_BUTTON.addEventListener("click", () => CARD.classList.toggle("flipped"));
 
-function flipCard() {
-  card.classList.toggle("flipped");
+function renderFlipBtn(){
+  const i = document.createElement('i');
+  i.id = "flip-btn";
+  i.classList.add('fa-solid', 'fa-repeat');
+  CARD.insertBefore(i, CARD.firstChild);
+  return i
 }
